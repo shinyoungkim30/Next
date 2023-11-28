6b7e2f7b6245d7e86828fe03a85b7534cd8c8a87
김신영 <ksyq12@gmail.com>
2023-11-28 11:18:28 +0900

    pre data fetching

---
+++ section04/pages/products/[pid].js
@@ -0,0 +1,61 @@
+import path from "path";
+import fs from "fs/promises";
+
+import { Fragment } from "react";
+
+const ProductDetailPage = (props) => {
+  const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loadding...</p>;
+  }
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+};
+
+const getData = async () => {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: true,
+  };
+};
+
+export default ProductDetailPage;
