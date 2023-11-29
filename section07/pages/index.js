c329c996a1f05fe12302c5d6035155796e91a4aa
김신영 <ksyq12@gmail.com>
2023-11-29 22:43:17 +0900

    metadata

---
+++ section07/pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+
+import EventList from '@/components/events/event-list';
+import { getFeaturedEvents } from '@/helpers/api-util';
+
+const HomePage = (props) => {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of greate events that allow you to evolve..."
+        />
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+
+  return { props: { events: featuredEvents }, revalidate: 1800 };
+};
+
+export default HomePage;
